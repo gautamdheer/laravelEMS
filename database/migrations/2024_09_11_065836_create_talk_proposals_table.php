ac9453c1a66234fab90797dfadfbd0480c6887e1
Gautam Dheer <gautam.litesh@gmail.com>
2024-09-11 14:10:48 +0530

    first commit

---
+++ database/migrations/2024_09_11_065836_create_talk_proposals_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('talk_proposals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('speaker_id')->constrained('speakers')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->json('tags');
+            $table->string('file_path');
+            $table->enum('status', ['Pending', 'Reviewed', 'Approved', 'Rejected'])->default('Pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('talk_proposals');
+    }
+};
